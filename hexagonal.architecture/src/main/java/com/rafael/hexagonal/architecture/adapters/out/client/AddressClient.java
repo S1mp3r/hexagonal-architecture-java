248625875d17b884d72c89c1d06c2d6c2cf54e0f
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-05 20:24:28 -0300

    feat(AddressClient): built AddressClient adapter

---
+++ hexagonal.architecture/src/main/java/com/rafael/hexagonal/architecture/adapters/out/client/AddressClient.java
@@ -0,0 +1,17 @@
+package com.rafael.hexagonal.architecture.adapters.out.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.hexagonal.architecture.adapters.out.client.response.AddressResponse;
+
+@RestController
+@FeignClient(name = "AddressClient", url = "${rafael.clients.address.url}")
+public interface AddressClient {
+    
+    @GetMapping("/{zipCode}")
+    public AddressResponse findAddressByZipCode(@PathVariable("zipCode") String zipCode);
+
+}
